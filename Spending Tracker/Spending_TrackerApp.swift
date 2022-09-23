ceefbf4ba31d360d7e190db9e460eea99e3da24a
Mario Elorza <mfem@Marios-MacBook-Pro.local>
2022-09-23 17:20:26 -0300

    Credit Card Paging Interface

---
+++ Spending Tracker/Spending_TrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Spending_TrackerApp.swift
+//  Spending Tracker
+//
+//  Created by Mario Elorza on 31-07-22.
+//
+
+import SwiftUI
+
+@main
+struct Spending_TrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
